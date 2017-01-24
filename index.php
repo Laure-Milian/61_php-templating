5a04b18a400007ce4d25ef6c5a6b57f7bebb3ce0
Laure <laure.milian@gmail.com>
2017-01-24 12:29:31 +0100

    structure de base

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Php Templating</title>
+</head>
+<body>
+BLA
+<?php require __DIR__ . '/vendor/autoload.php' ?>
+	
+</body>
+</html>
