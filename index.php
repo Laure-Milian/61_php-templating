a7a54d64a2d4f8419fbf7b14274a3dee9c1318d2
Laure <laure.milian@gmail.com>
2017-01-24 14:08:20 +0100

    genere template + infos faker (sauf product)

--- index.php
+++ index.php
@@ -5,8 +5,29 @@
 	<title>Php Templating</title>
 </head>
 <body>
-BLA
-<?php require __DIR__ . '/vendor/autoload.php' ?>
+	<?php require __DIR__ . '/vendor/autoload.php';
+	$faker = Faker\Factory::create();
 	
-</body>
-</html>
+	$data = [
+		'company' => $faker->company,
+		'catchPhrase' => $faker->catchPhrase,
+		'url' => $faker->url,
+		'color' => $faker->colorName,
+		'price' => $faker->randomDigitNotNull,
+		'userName' => $faker->name,
+		'job' => $faker->jobTitle,
+		'email' => $faker->email,
+		'telephone' =>  $faker->phoneNumber,
+		'address' => $faker->address,
+		'productAdjective' => $faker->word,
+		'productName' => $faker->word,
+		'productMaterial' => $faker->word
+	];
+
+	$loader = new Twig_Loader_Filesystem(__DIR__);
+	$twig = new Twig_Environment($loader);
+	echo $twig->render('profilePage.php', $data);
+		?>
+
+	</body>
+	</html>
