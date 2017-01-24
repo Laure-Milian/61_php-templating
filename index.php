31228ef65e4a2b28a6aa22b81feaf2d069598588
Laure <laure.milian@gmail.com>
2017-01-24 14:24:16 +0100

    affichage des infos - css reste à ajouter

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Php Templating</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
 </head>
 <body>
 	<?php require __DIR__ . '/vendor/autoload.php';
 	$faker = Faker\Factory::create();
-	
+
 	$data = [
 		'company' => $faker->company,
 		'catchPhrase' => $faker->catchPhrase,
@@ -21,7 +22,9 @@
 		'address' => $faker->address,
 		'productAdjective' => $faker->word,
 		'productName' => $faker->word,
-		'productMaterial' => $faker->word
+		'productMaterial' => $faker->word,
+		'companyImg' => $faker->imageUrl($width = 400, $height = 300),
+		'profileImg' => $faker->imageUrl($width = 50, $height = 50, 'cats')
 	];
 
 	$loader = new Twig_Loader_Filesystem(__DIR__);
